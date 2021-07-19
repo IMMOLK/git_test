25867ee1cfc0e6b072aa7ff549251373f5f09483
“IMMOLK” <1499622664@qq.com>
2021-07-19 10:10:29 +0800

    master test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("Hello git2");
         System.out.println("Hello git3");
         System.out.println("Hello hot-fix");
+        System.out.println("master test");
     }
 
 
