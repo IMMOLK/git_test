e9e95825404e778415618eb5536ac8e8939e618d
IMMOLK <75161356+IMMOLK@users.noreply.github.com>
2021-07-19 10:42:32 +0800

    pull test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("Hello hot-fix");
         System.out.println("hot-fix test");
         System.out.println("master push test");
+        System.out.println("master pull test");
     }
 
 
