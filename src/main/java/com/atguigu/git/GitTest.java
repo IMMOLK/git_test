3c7140bc5ec1ee13050935bf479ee964bce5dd6a
“IMMOLK” <1499622664@qq.com>
2021-07-19 10:29:47 +0800

    hot-fix push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello git3");
         System.out.println("Hello hot-fix");
         System.out.println("hot-fix test");
+        System.out.println("push hot-fix test");
     }
 
 
